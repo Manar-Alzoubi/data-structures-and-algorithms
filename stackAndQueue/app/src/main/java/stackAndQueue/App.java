5791bcca9b84ab0c4d0832e23bbfea86004f582c
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-27 22:40:31 +0300

    code 14 done

--- stackAndQueue/app/src/main/java/stackAndQueue/App.java
+++ stackAndQueue/app/src/main/java/stackAndQueue/App.java
@@ -5,21 +5,37 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-      ValidateBrackets isValid = new ValidateBrackets();
+      /////// code 14 ///////////////
 
-        System.out.println("{}  ==> " + isValid.validateBrackets("{}"));
-        System.out.println("{}(){}  ==> " + isValid.validateBrackets("{}(){}"));
-        System.out.println("()[[Extra Characters]]  ==> " + isValid.validateBrackets("()[[Extra Characters]]"));
-        System.out.println("(){}[[]]  ==> " + isValid.validateBrackets("(){}[[]]"));
-        System.out.println("{}{Code}[Fellows](())  ==> " + isValid.validateBrackets("{}{Code}[Fellows](())"));
-        System.out.println("[({}]  ==> " + isValid.validateBrackets("[({}]"));
-        System.out.println("(](  ==> " + isValid.validateBrackets("(]("));
-        System.out.println("{(})  ==> " + isValid.validateBrackets("{(})"));
-        System.out.println("(  ==> " + isValid.validateBrackets("("));
-        System.out.println("}  ==> " + isValid.validateBrackets("}"));
-        System.out.println("(} ==> " + isValid.validateBrackets("(}"));
-        System.out.println("(Sadan} ==> " + isValid.validateBrackets("(Sadan}"));
-        System.out.println("(Sa]dan} ==> " + isValid.validateBrackets("(Sa]dan}"));
+
+        Stack<Integer> stack= new Stack();
+        stack.push(5);
+        stack.push(2);
+        stack.push(7);
+        stack.push(9);
+        stack.push(2);
+      System.out.println("max num is ==>  "+ stack.getMax());
+
+
+
+
+
+
+//      ValidateBrackets isValid = new ValidateBrackets();
+//
+//        System.out.println("{}  ==> " + isValid.validateBrackets("{}"));
+//        System.out.println("{}(){}  ==> " + isValid.validateBrackets("{}(){}"));
+//        System.out.println("()[[Extra Characters]]  ==> " + isValid.validateBrackets("()[[Extra Characters]]"));
+//        System.out.println("(){}[[]]  ==> " + isValid.validateBrackets("(){}[[]]"));
+//        System.out.println("{}{Code}[Fellows](())  ==> " + isValid.validateBrackets("{}{Code}[Fellows](())"));
+//        System.out.println("[({}]  ==> " + isValid.validateBrackets("[({}]"));
+//        System.out.println("(](  ==> " + isValid.validateBrackets("(]("));
+//        System.out.println("{(})  ==> " + isValid.validateBrackets("{(})"));
+//        System.out.println("(  ==> " + isValid.validateBrackets("("));
+//        System.out.println("}  ==> " + isValid.validateBrackets("}"));
+//        System.out.println("(} ==> " + isValid.validateBrackets("(}"));
+//        System.out.println("(Sadan} ==> " + isValid.validateBrackets("(Sadan}"));
+//        System.out.println("(Sa]dan} ==> " + isValid.validateBrackets("(Sa]dan}"));
 
 //        AnimalShelter allAnimals = new AnimalShelter();
 //        Dog dog = new Dog("dog1");
